8ef45aba4f1eb3fa2d05fcbe0022786e0757c857
willelz <37827548+willelz@users.noreply.github.com>
2018-05-14 23:50:16 +0900

    add crane

--- app.ts
+++ app.ts
@@ -1,19 +1,20 @@
-import cranes, {line} from './crane'
+import cranes, { line } from "./crane";
 
-const selectCrane = (range: number, weight: number) =>{
-  let candidate: string[] = []
-  for(const crane of cranes){
-    let justLine: line = {"range":0, "maxLoad":0};
-    for(const line of crane.GrossRatedLoad){
-      if(line.range >= range || line.maxLoad <= weight)
-        break;
+const selectCrane = (range: number, weight: number) => {
+  let candidate: string[] = [];
+  for (const crane of cranes) {
+    let justLine: line = { range: 0, maxLoad: 0 };
+    for (const line of crane.GrossRatedLoad) {
+      if (line.range >= range || line.maxLoad <= weight) break;
       justLine = line;
     }
-    const res = `${crane.name} : 作業半径${justLine.range}M時の定格荷重は${justLine.maxLoad}トン`
+    const res = `${crane.name} : 作業半径${justLine.range}M時の定格荷重は${
+      justLine.maxLoad
+    }トン`;
     candidate.push(res);
   }
   return candidate;
-}
+};
 
 const test = selectCrane(10, 2);
-console.log(test)
+console.log(test);
